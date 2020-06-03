c2846712e0b6a639422f6ab681ca4c0804130abe
Your Name <you@example.com>
2020-06-03 14:38:31 -0700

    all the hand types function

--- src/Five_Cards_Hand.java
+++ src/Five_Cards_Hand.java
@@ -20,16 +20,11 @@ public class Five_Cards_Hand {
 		return newarr;
 	}//Append an element at the end of array
 	
-	
-	//public char[] suit_array(){
-	//	char[] suits = {suit1, suit2, suit3, suit4, suit5};
-	//	return suits;
-	//}An array that contains suits of a 5 cards hand
-	
-	//public char[] card_array(){
-	//	char[] suits = {card1, card2, card3, card4, card5};
-	//	return suits;
-	//}An array that contains cards of a 5 cards hand
+	public int cardconverter(char x) {
+		if (x=='6') {return 6;}else if (x=='7') {return 7;}else if (x=='8') {return 8;}else if (x=='9') {
+			return 9;}else if (x=='T') {return 10;}else if (x=='J') {return 11;}else if (x=='Q') {
+			return 12;}else if (x=='K') {return 13;} else if (x=='A') {return 14;}else {return -100;}
+	}//Convert card in char to number e.g. '6'->6, 'T'->10, 'Q'->12, 'A'->14
 	
 	public int[] pattern(char[] A){
 		int[] pattern= {};
@@ -57,9 +52,65 @@ public class Five_Cards_Hand {
 		char[] suits = {suit1, suit2, suit3, suit4, suit5};
 		int[] suitpattern = pattern(suits);
 		int[] flushpattern = {5};
-		if (Arrays.equals(suitpattern,flushpattern)) {return true;
-		}else {return false;}
-	}//Check if the 5 cards hand is a flush
+		return Arrays.equals(suitpattern,flushpattern);
+	}//Check if the given 5 cards hand is a flush (include straight flush)
+
+	public boolean isstraight() {
+		char[] cards = {card1, card2, card3, card4, card5};
+		int[] cardsrank = {};
+		int i;
+		for (i=0; i<cards.length; i++) {cardsrank = addX(cardsrank.length, cardsrank, cardconverter(cards[i]));}
+		Arrays.sort(cardsrank);
+		int[] wheel = {6,7,8,9,14};
+		boolean out=true;
+		for (i=0;i<4;i++) {out = out && (cardsrank[i]+1==cardsrank[i+1]);}
+		if (Arrays.equals(cardsrank, wheel)) {out = true;}
+		return out;
+	}//Check if the given 5 cards hand is a straight (include straight flush)
 	
-	
+	public boolean isstraightflush() {
+		return isflush() && isstraight();
+	}//Check if the given 5 cards hand is a straight flush
+	public boolean isjustflush() {
+		return isflush() && !isstraight();
+	}//Check if the given 5 cards hand is a flush (not straight flush)
+	public boolean isjuststraight() {
+		return !isflush() && isstraight();
+	}//Check if the given 5 cards hand is a straght (not straight flush)
+	public boolean isquad() {
+		char[] cards = {card1, card2, card3, card4, card5};
+		int[] cardpattern = pattern(cards);
+		int[] targetpattern = {1,4};
+		return (Arrays.equals(cardpattern,targetpattern));
+	}
+	public boolean isfullhouse() {
+		char[] cards = {card1, card2, card3, card4, card5};
+		int[] cardpattern = pattern(cards);
+		int[] targetpattern = {2,3};
+		return (Arrays.equals(cardpattern,targetpattern));
+	}
+	public boolean istrips() {
+		char[] cards = {card1, card2, card3, card4, card5};
+		int[] cardpattern = pattern(cards);
+		int[] targetpattern = {1,1,3};
+		return (Arrays.equals(cardpattern,targetpattern));
+	}
+	public boolean istwopair() {
+		char[] cards = {card1, card2, card3, card4, card5};
+		int[] cardpattern = pattern(cards);
+		int[] targetpattern = {1,2,2};
+		return (Arrays.equals(cardpattern,targetpattern));
+	}
+	public boolean isonepair() {
+		char[] cards = {card1, card2, card3, card4, card5};
+		int[] cardpattern = pattern(cards);
+		int[] targetpattern = {1,1,1,2};
+		return (Arrays.equals(cardpattern,targetpattern));
+	}
+	public boolean ishighcard() {
+		char[] cards = {card1, card2, card3, card4, card5};
+		int[] cardpattern = pattern(cards);
+		int[] targetpattern = {1,1,1,1,1};
+		return (Arrays.equals(cardpattern,targetpattern));
+	}//A bunch of boolean functions on poker hand type
 }
--- src/Main_Flopzilla.java
+++ src/Main_Flopzilla.java
@@ -1,7 +1,7 @@
 //This will be the main program
 public class Main_Flopzilla {
 	public static void main(String[] args) {
-		Five_Cards_Hand myhand = new Five_Cards_Hand('6','s','7','h','9','h','T','h','K','h');
-		System.out.println(myhand.isflush());
+		Five_Cards_Hand myhand = new Five_Cards_Hand('Q','h','K','s','Q','c','6','d','K','d');
+		System.out.println(myhand.istwopair());
 	}
 }

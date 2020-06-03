601b20d43e7d1bdf4902da4d9d0fdceb2e083d78
Your Name <you@example.com>
2020-06-03 01:51:39 -0700

    add isFlush function

--- src/Five_Cards_Hand.java
+++ src/Five_Cards_Hand.java
@@ -10,7 +10,56 @@ public class Five_Cards_Hand {
 		suit1=s1; suit2=s2; suit3=s3; suit4=s4; suit5=s5;
 	}//Constructor
 	
+	public static int[] addX(int n, int arr[], int x) {
+		int i;
+		int newarr[] = new int[n+1];
+		for (i=0; i<n; i++) {
+			newarr[i] = arr[i];
+		}
+		newarr[n] = x;
+		return newarr;
+	}//Append an element at the end of array
 	
 	
-
+	//public char[] suit_array(){
+	//	char[] suits = {suit1, suit2, suit3, suit4, suit5};
+	//	return suits;
+	//}An array that contains suits of a 5 cards hand
+	
+	//public char[] card_array(){
+	//	char[] suits = {card1, card2, card3, card4, card5};
+	//	return suits;
+	//}An array that contains cards of a 5 cards hand
+	
+	public int[] pattern(char[] A){
+		int[] pattern= {};
+		int i;
+		
+		HashMap<Character, Integer> cardcount = new HashMap<Character, Integer>();
+		
+		for (i=0; i<A.length; i++) {
+			if (cardcount.containsKey(A[i])) {
+				cardcount.put(A[i], cardcount.get(A[i])+1);
+			} else {
+				cardcount.put(A[i], 1);
+			}
+		}
+		
+		for (Integer value : cardcount.values()) {
+			pattern = addX(pattern.length,pattern,value);
+		}
+		
+		Arrays.sort(pattern);
+		return pattern;//Return pattern e.g. {"pig","dog","pig","cat","pig"} pattern will be {1,1,3}
+	}
+	
+	public boolean isflush() {
+		char[] suits = {suit1, suit2, suit3, suit4, suit5};
+		int[] suitpattern = pattern(suits);
+		int[] flushpattern = {5};
+		if (Arrays.equals(suitpattern,flushpattern)) {return true;
+		}else {return false;}
+	}//Check if the 5 cards hand is a flush
+	
+	
 }
